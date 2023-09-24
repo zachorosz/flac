5f7870189072caa7e85691cbcd32009cd75733fb
Zach Orosz <zjo5002@gmail.com>
2023-09-24 18:46:37 -0400

    cleanup metaflac output

--- cmd/metaflac/main.go
+++ cmd/metaflac/main.go
@@ -63,72 +63,72 @@ func listMetadata(w io.Writer, prefix, file string) {
 		readLast = b.Last
 
 		fmt.Fprintf(w, "%sMETADATA block #%d\n", prefix, i+1)
-		fmt.Fprintf(w, "%s\ttype: %d (%s)\n", prefix, b.Type, b.Type)
-		fmt.Fprintf(w, "%s\tis last: %t\n", prefix, b.Last)
-		fmt.Fprintf(w, "%s\tlength: %d\n", prefix, b.Length)
+		fmt.Fprintf(w, "%s type: %d (%s)\n", prefix, b.Type, b.Type)
+		fmt.Fprintf(w, "%s is last: %t\n", prefix, b.Last)
+		fmt.Fprintf(w, "%s length: %d\n", prefix, b.Length)
 
 		switch b := b.Data.(type) {
 		case *flac.StreamInfo:
-			fmt.Fprintf(w, "%s\tminimum block size: %d samples\n", prefix, b.MinimumBlockSize)
-			fmt.Fprintf(w, "%s\tmaximum block size: %d samples\n", prefix, b.MaximumBlockSize)
-			fmt.Fprintf(w, "%s\tminimum frame size: %d bytes\n", prefix, b.MinimumFrameSize)
-			fmt.Fprintf(w, "%s\tmaximum frame size: %d bytes\n", prefix, b.MaximumFrameSize)
-			fmt.Fprintf(w, "%s\tsample_rate: %d Hz\n", prefix, b.SampleRate)
-			fmt.Fprintf(w, "%s\tchannels: %d\n", prefix, b.Channels)
-			fmt.Fprintf(w, "%s\tbits-per-sample: %d\n", prefix, b.BitsPerSample)
-			fmt.Fprintf(w, "%s\ttotal samples: %d\n", prefix, b.TotalSamples)
-			fmt.Fprintf(w, "%s\tMD5 signature: %x\n", prefix, b.MD5)
+			fmt.Fprintf(w, "%s minimum block size: %d samples\n", prefix, b.MinimumBlockSize)
+			fmt.Fprintf(w, "%s maximum block size: %d samples\n", prefix, b.MaximumBlockSize)
+			fmt.Fprintf(w, "%s minimum frame size: %d bytes\n", prefix, b.MinimumFrameSize)
+			fmt.Fprintf(w, "%s maximum frame size: %d bytes\n", prefix, b.MaximumFrameSize)
+			fmt.Fprintf(w, "%s sample_rate: %d Hz\n", prefix, b.SampleRate)
+			fmt.Fprintf(w, "%s channels: %d\n", prefix, b.Channels)
+			fmt.Fprintf(w, "%s bits-per-sample: %d\n", prefix, b.BitsPerSample)
+			fmt.Fprintf(w, "%s total samples: %d\n", prefix, b.TotalSamples)
+			fmt.Fprintf(w, "%s MD5 signature: %x\n", prefix, b.MD5)
 		case *flac.Application:
-			fmt.Fprintf(w, "%s\tapplication id: %s\n", prefix, b.ID)
-			fmt.Fprintf(w, "%s\tapplication data: %x\n", prefix, b.Data)
+			fmt.Fprintf(w, "%s application id: %s\n", prefix, b.ID)
+			fmt.Fprintf(w, "%s application data: %x\n", prefix, b.Data)
 		case *flac.SeekTable:
-			fmt.Fprintf(w, "%s\tseek points: %d\n", prefix, len(b.SeekPoints))
+			fmt.Fprintf(w, "%s seek points: %d\n", prefix, len(b.SeekPoints))
 			for i, p := range b.SeekPoints {
-				fmt.Fprintf(w, "%s\t\tpoint %d: sample_number=%d, stream_offset=%d, frame_samples=%d\n", prefix, i, p.SampleNumber, p.Offset, p.NumSamples)
+				fmt.Fprintf(w, "%s  point %d: sample_number=%d, stream_offset=%d, frame_samples=%d\n", prefix, i, p.SampleNumber, p.Offset, p.NumSamples)
 			}
 		case *flac.VorbisComment:
-			fmt.Fprintf(w, "%s\tvendor string: %s\n", prefix, b.Vendor)
-			fmt.Fprintf(w, "%s\tcomments: %d\n", prefix, len(b.UserComments))
+			fmt.Fprintf(w, "%s vendor string: %s\n", prefix, b.Vendor)
+			fmt.Fprintf(w, "%s comments: %d\n", prefix, len(b.UserComments))
 			for i, c := range b.UserComments {
-				fmt.Fprintf(w, "%s\t\tcomment[%d]: %s\n", prefix, i, c)
+				fmt.Fprintf(w, "%s  comment[%d]: %s\n", prefix, i, c)
 			}
 		case *flac.CueSheet:
-			fmt.Fprintf(w, "%s\tmedia catalog number: %s\n", prefix, b.CatalogNumber)
-			fmt.Fprintf(w, "%s\tlead-in: %d\n", prefix, b.NumLeadInSamples)
-			fmt.Fprintf(w, "%s\tis CD: %t\n", prefix, b.IsCD)
-			fmt.Fprintf(w, "%s\tnumber of tracks: %d\n", prefix, len(b.Tracks))
+			fmt.Fprintf(w, "%s media catalog number: %s\n", prefix, b.CatalogNumber)
+			fmt.Fprintf(w, "%s lead-in: %d\n", prefix, b.NumLeadInSamples)
+			fmt.Fprintf(w, "%s is CD: %t\n", prefix, b.IsCD)
+			fmt.Fprintf(w, "%s number of tracks: %d\n", prefix, len(b.Tracks))
 			for i, t := range b.Tracks {
-				fmt.Fprintf(w, "%s\t\ttrack[%d]\n", prefix, i)
-				fmt.Fprintf(w, "%s\t\t\toffset: %d\n", prefix, t.OffsetSamples)
-				fmt.Fprintf(w, "%s\t\t\tnumber: %d\n", prefix, t.TrackNumber)
-				fmt.Fprintf(w, "%s\t\t\tISRC: %s\n", prefix, t.ISRC)
+				fmt.Fprintf(w, "%s  track[%d]\n", prefix, i)
+				fmt.Fprintf(w, "%s   offset: %d\n", prefix, t.OffsetSamples)
+				fmt.Fprintf(w, "%s   number: %d\n", prefix, t.TrackNumber)
+				fmt.Fprintf(w, "%s   ISRC: %s\n", prefix, t.ISRC)
 				if t.IsAudio {
-					fmt.Fprintf(w, "%s\t\t\ttype: AUDIO\n", prefix)
+					fmt.Fprintf(w, "%s   type: AUDIO\n", prefix)
 				} else {
-					fmt.Fprintf(w, "%s\t\t\ttype: NON-AUDIO\n", prefix)
+					fmt.Fprintf(w, "%s   type: NON-AUDIO\n", prefix)
 				}
-				fmt.Fprintf(w, "%s\t\t\tpre-emphasis: %t\n", prefix, t.PreEmphasis)
-				fmt.Fprintf(w, "%s\t\t\tnumber of index points: %d\n", prefix, len(t.Indices))
+				fmt.Fprintf(w, "%s   pre-emphasis: %t\n", prefix, t.PreEmphasis)
+				fmt.Fprintf(w, "%s   number of index points: %d\n", prefix, len(t.Indices))
 
 				for j, p := range t.Indices {
-					fmt.Fprintf(w, "%s\t\t\t\tindex[%d]\n", prefix, j)
-					fmt.Fprintf(w, "%s\t\t\t\t\toffset: %d\n", prefix, p.OffsetSamples)
-					fmt.Fprintf(w, "%s\t\t\t\t\tnumber: %d\n", prefix, p.PointNumber)
+					fmt.Fprintf(w, "%s    index[%d]\n", prefix, j)
+					fmt.Fprintf(w, "%s     offset: %d\n", prefix, p.OffsetSamples)
+					fmt.Fprintf(w, "%s     number: %d\n", prefix, p.PointNumber)
 				}
 			}
 		case *flac.Picture:
-			fmt.Fprintf(w, "%s\ttype: %d (%s)\n", prefix, b.Type, b.Type)
-			fmt.Fprintf(w, "%s\tMIME type: %s\n", prefix, b.MimeType)
-			fmt.Fprintf(w, "%s\tdescription: %s\n", prefix, b.Description)
-			fmt.Fprintf(w, "%s\twidth: %d\n", prefix, b.Width)
-			fmt.Fprintf(w, "%s\theight: %d\n", prefix, b.Height)
-			fmt.Fprintf(w, "%s\tdepth: %d\n", prefix, b.Depth)
+			fmt.Fprintf(w, "%s type: %d (%s)\n", prefix, b.Type, b.Type)
+			fmt.Fprintf(w, "%s MIME type: %s\n", prefix, b.MimeType)
+			fmt.Fprintf(w, "%s description: %s\n", prefix, b.Description)
+			fmt.Fprintf(w, "%s width: %d\n", prefix, b.Width)
+			fmt.Fprintf(w, "%s height: %d\n", prefix, b.Height)
+			fmt.Fprintf(w, "%s depth: %d\n", prefix, b.Depth)
 			if b.Colors == 0 {
-				fmt.Fprintf(w, "%s\tcolors: 0 (unindexed)\n", prefix)
+				fmt.Fprintf(w, "%s colors: 0 (unindexed)\n", prefix)
 			} else {
-				fmt.Fprintf(w, "%s\tcolors: %d\n", prefix, b.Colors)
+				fmt.Fprintf(w, "%s colors: %d\n", prefix, b.Colors)
 			}
-			fmt.Fprintf(w, "%s\tdata length: %d\n", prefix, len(b.Data))
+			fmt.Fprintf(w, "%s data length: %d\n", prefix, len(b.Data))
 		}
 
 		i++
