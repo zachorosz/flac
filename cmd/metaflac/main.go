fe1d4bdea919b916c9adb19e2873f0e4e5718d0c
Zach Orosz <zjo5002@gmail.com>
2023-09-24 15:55:42 -0400

    metaflac binary

---
+++ cmd/metaflac/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/zachorosz/flac"
+)
+
+func help(w io.Writer) {
+	fmt.Fprintln(w, `Usage:
+    metaflac FLACfile [FLACfile ...]
+
+List metadata in one or more FLAC files.`)
+}
+
+func fatalf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(1)
+}
+
+func main() {
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	if len(os.Args) < 2 {
+		help(out)
+		os.Exit(1)
+	}
+	flacFiles := os.Args[1:]
+
+	list(out, flacFiles)
+}
+
+func list(w io.Writer, files []string) {
+	for _, f := range files {
+		var prefix string
+		if len(files) > 1 {
+			prefix = fmt.Sprintf("%s:", f)
+		}
+		listMetadata(w, prefix, f)
+	}
+}
+
+func listMetadata(w io.Writer, prefix, file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		fatalf("%s: failed to open FLAC file", file)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	r := flac.NewReader(f)
+
+	i := 0
+	for readLast := false; !readLast; {
+		b, err := r.ReadBlock()
+		if err != nil {
+			fatalf("%s: failed to read block: %v", file, err)
+		}
+		readLast = b.Last
+
+		fmt.Fprintf(w, "%sMETADATA block #%d\n", prefix, i+1)
+		fmt.Fprintf(w, "%s\ttype: %d (%s)\n", prefix, b.Type, b.Type)
+		fmt.Fprintf(w, "%s\tis last: %t\n", prefix, b.Last)
+		fmt.Fprintf(w, "%s\tlength: %d\n", prefix, b.Length)
+
+		switch b := b.Data.(type) {
+		case *flac.StreamInfo:
+			fmt.Fprintf(w, "%s\tminimum block size: %d samples\n", prefix, b.MinimumBlockSize)
+			fmt.Fprintf(w, "%s\tmaximum block size: %d samples\n", prefix, b.MaximumBlockSize)
+			fmt.Fprintf(w, "%s\tminimum frame size: %d bytes\n", prefix, b.MinimumFrameSize)
+			fmt.Fprintf(w, "%s\tmaximum frame size: %d bytes\n", prefix, b.MaximumFrameSize)
+			fmt.Fprintf(w, "%s\tsample_rate: %d Hz\n", prefix, b.SampleRate)
+			fmt.Fprintf(w, "%s\tchannels: %d\n", prefix, b.Channels)
+			fmt.Fprintf(w, "%s\tbits-per-sample: %d\n", prefix, b.BitsPerSample)
+			fmt.Fprintf(w, "%s\ttotal samples: %d\n", prefix, b.TotalSamples)
+			fmt.Fprintf(w, "%s\tMD5 signature: %x\n", prefix, b.MD5)
+		case *flac.Application:
+			fmt.Fprintf(w, "%s\tapplication id: %s\n", prefix, b.ID)
+			fmt.Fprintf(w, "%s\tapplication data: %x\n", prefix, b.Data)
+		case *flac.SeekTable:
+			fmt.Fprintf(w, "%s\tseek points: %d\n", prefix, len(b.SeekPoints))
+			for i, p := range b.SeekPoints {
+				fmt.Fprintf(w, "%s\t\tpoint %d: sample_number=%d, stream_offset=%d, frame_samples=%d\n", prefix, i, p.SampleNumber, p.Offset, p.NumSamples)
+			}
+		case *flac.VorbisComment:
+			fmt.Fprintf(w, "%s\tvendor string: %s\n", prefix, b.Vendor)
+			fmt.Fprintf(w, "%s\tcomments: %d\n", prefix, len(b.UserComments))
+			for i, c := range b.UserComments {
+				fmt.Fprintf(w, "%s\t\tcomment[%d]: %s\n", prefix, i, c)
+			}
+		case *flac.CueSheet:
+		case *flac.Picture:
+			fmt.Fprintf(w, "%s\ttype: %d (%s)\n", prefix, b.Type, b.Type)
+			fmt.Fprintf(w, "%s\tMIME type: %s\n", prefix, b.MimeType)
+			fmt.Fprintf(w, "%s\tdescription: %s\n", prefix, b.Description)
+			fmt.Fprintf(w, "%s\twidth: %d\n", prefix, b.Width)
+			fmt.Fprintf(w, "%s\theight: %d\n", prefix, b.Height)
+			fmt.Fprintf(w, "%s\tdepth: %d\n", prefix, b.Depth)
+			if b.Colors == 0 {
+				fmt.Fprintf(w, "%s\tcolors: 0 (unindexed)\n", prefix)
+			} else {
+				fmt.Fprintf(w, "%s\tcolors: %d\n", prefix, b.Colors)
+			}
+			fmt.Fprintf(w, "%s\tdata length: %d\n", prefix, len(b.Data))
+		}
+
+		i++
+	}
+}
